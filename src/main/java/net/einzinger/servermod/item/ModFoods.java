6587540274d7e00ef919855fd30f8c5209c85cbf
Julian Einzinger <julian5einzinger@gmail.com>
2024-07-20 15:50:10 +0200

    Added Blueberry + Food behaviour

---
+++ src/main/java/net/einzinger/servermod/item/ModFoods.java
@@ -0,0 +1,9 @@
+package net.einzinger.servermod.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+
+    public static final FoodProperties BLUEBERRY = (new FoodProperties.Builder()).nutrition(2).saturationMod(.2f).fast().build();
+
+}
